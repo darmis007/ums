c58e2a782192679a37d375ab891630adb10bd701
Darsh <darshmishra3010@gmail.com>
2020-01-06 00:44:32 +0530

    change settings

--- utkarsh/utkarsh/settings.py
+++ utkarsh/utkarsh/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zxp*sof*nsesity6^jsa_3-9(z&v0itr3_l3jwqsf&m$*^&m0f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
